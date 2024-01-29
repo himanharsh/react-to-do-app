831a95dd1641d5a939e737506fc0620885143d46
Himanshu <himanshuharsh1999@gmail.com>
2024-01-29 19:12:38 +0530

    empty task cannot be added feature implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { FaCheck, FaTrash, FaPlus } from 'react-icons/fa';
+import { FaCheck, FaTrashAlt, FaPlus } from 'react-icons/fa';
 import { useState } from 'react';
 
 let nextId = 0;
@@ -10,15 +10,20 @@ const TaskCount = ({ taskArr }) => {
     if (x.done === false) count++;
   });
   if (count === 1) {
-    return <div>1 TASK LEFT</div>;
-  } else return <div>{count} TASKS LEFT</div>;
+    return <div className="taskCounter">1 TASK LEFT</div>;
+  } else return <div className="taskCounter">{count} TASKS LEFT</div>;
 };
 
 const App = () => {
   const [task, setTask] = useState('');
   const [taskList, setTaskList] = useState([]);
   function add() {
+    if (task === '') {
+      alert('Please Enter a Task Before Adding It!');
+      return;
+    }
     setTaskList([...taskList, { id: nextId++, task: task, done: false }]);
+    setTask('');
   }
   function trash(t) {
     setTaskList(taskList.filter((tl) => tl.id !== t.id));
@@ -38,41 +43,45 @@ const App = () => {
   }
 
   return (
-    <main>
-      <input
-        type="text"
-        placeholder="Enter your task here..."
-        value={task}
-        onChange={(e) => setTask(e.target.value)}
-        onKeyUp={(e) => {
-          if (e.key === 'Enter') add();
-        }}
-      />
-      <button onClick={add}>
-        <FaPlus />
-      </button>
-      <ul>
-        {taskList.map((t) => (
-          <li
-            key={t.id}
-            style={
-              t.done
-                ? { textDecoration: 'line-through' }
-                : { textDecoration: 'none' }
-            }
-          >
-            {t.task}
-            <button onClick={() => checkToggle(t)}>
-              <FaCheck />
-            </button>
-            <button onClick={() => trash(t)}>
-              <FaTrash />
-            </button>
-          </li>
-        ))}
-      </ul>
+    <>
+      <main>
+        <input
+          type="text"
+          placeholder="Enter your task here..."
+          value={task}
+          onChange={(e) => setTask(e.target.value)}
+          onKeyUp={(e) => {
+            if (e.key === 'Enter') add();
+          }}
+        />
+        <button onClick={add}>
+          <FaPlus />
+        </button>
+        <ul>
+          {taskList.map((t) => (
+            <li key={t.id}>
+              <span
+                className="taskName"
+                style={
+                  t.done
+                    ? { textDecoration: 'line-through' }
+                    : { textDecoration: 'none' }
+                }
+              >
+                {t.task}
+              </span>
+              <button className="checkIcon" onClick={() => checkToggle(t)}>
+                <FaCheck />
+              </button>
+              <button className="trashIcon" onClick={() => trash(t)}>
+                <FaTrashAlt />
+              </button>
+            </li>
+          ))}
+        </ul>
+      </main>
       <TaskCount taskArr={taskList} />
-    </main>
+    </>
   );
 };
 
